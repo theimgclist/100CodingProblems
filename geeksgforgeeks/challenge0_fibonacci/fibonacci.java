109a6361663f5bfe2f098a27ea7c476fd15be8c9
Avinash <avinash.kappa@gmail.com>
2017-07-21 13:08:04 +0530

    challenge0_fibonacci: Modified java solution

--- geeksgforgeeks/challenge0_fibonacci/fibonacci.java
+++ geeksgforgeeks/challenge0_fibonacci/fibonacci.java
@@ -6,44 +6,48 @@ import java.util.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-public class fibonacci 
+public class Fibonacci 
 {
 
 	static int memo[]; // instead of declaring the array size with some value, we can do it after taking the input, to save space
         public static void main(String[] args) 
 	{
         	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int nth_term = 0,result = 0;
+		int nth_term = 0,nth_fib_number = 0;
 		try
 		{       nth_term = Integer.parseInt(br.readLine());
 			memo = new int[nth_term]; //array size declared here
 			Arrays.fill(memo,-1);
-		//	result = nth_fib_number_memoization(nth_term-1);
-			result = nth_fib_number_tabulation(nth_term);
+			Process link = new Process();
+			//nth_fib_number = link.nth_fib_number_memoization(nth_term-1, memo);
+			nth_fib_number = link.nth_fib_number_tabulation(nth_term,memo);
 		
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
-                System.out.println("The "+nth_term+"th term in Fibonacci series is "+result);
+                System.out.println("The "+nth_term+"th term in Fibonacci series is "+nth_fib_number);
 	}
+}
 
-        public static int nth_fib_number_memoization(int n)
+class Process
+{
+
+        public int nth_fib_number_memoization(int n, int[] memo)
 	{
 		if(memo[n] == -1)
          	{
          		if(n <= 1)
          			memo[n] = n;
          		else
-         			memo[n] = nth_fib_number_memoization(n-1) + nth_fib_number_memoization(n-2);
+         			memo[n] = nth_fib_number_memoization(n-1,memo) + nth_fib_number_memoization(n-2,memo);
          	}
          	return memo[n];	
 
         }
-	public static int nth_fib_number_tabulation(int n)
+	public int nth_fib_number_tabulation(int n, int[] table)
 	{
-		int table[] = new int[n];
 		table[0] = 0;
 		table[1] = 1;
 		for(int i = 2; i < n; i++)
