bd9ef1e6a2b85d4e2229c7ddf9d171f551fb4d37
Avinash <avinash.kappa@gmail.com>
2017-09-21 13:54:05 +0530

    Added challenge8_knapsack : With Python solution

---
+++ geeksgforgeeks/challenge8_knapsack/README.md
@@ -0,0 +1,8 @@
+[http://www.geeksforgeeks.org/knapsack-problem/]  
+Problem definition: Suppose we are given a bag/knapsack which can contain items upto weight 'k_w'.  
+We are then given the list of items 'n' along with their weights 'i_w' and values 'i_v'.  
+We should choose the items in such a way that the capacity of the bag is maximised and is within the weight limit 'k_w'.  
+To better understand this, imagine you are on a treasure hunt and found gold coins of different weights.  
+Unfortunately for you, you can only choose coins whose total weight will be within your backpack's weight limit.  
+How will you go about choosing the right coins?  
+The blog post [http://javabypatel.blogspot.in/2016/09/knapsack-problem-solution-recursive-iterative.html] has a very good explanation with example.  
---
+++ geeksgforgeeks/challenge8_knapsack/knapsack.py
@@ -0,0 +1,44 @@
+# Created by Avinash on 20th Sept, 2017
+def process():
+	k_w = int(input("Enter the weight of knapsack: "))
+	n = int(input("Enter number of items : "))
+	i_w = [ 0 for i in range(n+1)]
+	i_v = [ 0 for j in range(n+1)]
+	print("Enter the item weights")
+	for i in range(n):
+		i_w[i] = int(input())
+	print("Enter the item values")	
+	for i in range(n):
+		i_v[i] = int(input())
+	capacity = knapsack(k_w,n,i_w,i_v)
+	print("The capacity of knapsack for given items is %-5d" %capacity)
+
+def knapsack_recursion(k_w,n,i_w,i_v):
+	if n == 0 or k_w == 0 :
+		return 0
+	if i_w[n-1] > k_w :
+		return knapsack_recursion(k_w,n-1,i_w,i_v)
+	else :
+		return max(i_v[n-1] + knapsack_recursion(k_w - i_w[n-1],n-1,i_w,i_v),
+		knapsack_recursion(k_w,n-1,i_w,i_v))
+
+def knapsack(k_w,n,i_w,i_v):
+	cache = [[ 0 for j in range(k_w + 1)] for i in range(n + 1)]
+	for i in range(n+1):
+		for j in range(k_w + 1):
+			if i == 0 or j == 0:
+				cache[i][j] = 0
+			elif i_w[i - 1] <= k_w :
+				cache[i][j] = max(i_v[i] + cache[i-1][k_w - i_w[i-1]],
+				cache[i-1][k_w])
+			else :
+				cache[i-1][k_w]
+	return cache[n][k_w]
+			
+
+if __name__ == "__main__":
+	process()
+	
+	
+	
+	
